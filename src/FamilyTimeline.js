3f338cb0bf54fda611119f007fd1dba2b958d55d
meghaman <mkulacha@gmail.com>
2025-05-10 16:27:15 -0400

    More content

--- src/FamilyTimeline.js
+++ src/FamilyTimeline.js
@@ -16,6 +16,12 @@ import {
   FaHome,
   FaRing,
   FaPlus,
+  FaBaby,
+  FaStarOfDavid,
+  FaSynagogue,
+  FaWalking,
+  FaHiking,
+  FaUmbrellaBeach,
 } from "react-icons/fa";
 
 import Mar_11_2020 from "./images/Mar_11_2020.jpg";
@@ -32,6 +38,15 @@ import Apr_3_2021 from "./images/Apr_3_2021.jpg";
 import May_22_2021 from "./images/May_22_2021.jpg";
 import Jun_4_2021 from "./images/Jun_4_2021.jpg";
 import Aug_12_2021 from "./images/Aug_12_2021.jpg";
+import Feb_12_2022 from "./images/Feb_19_2022.jpg";
+import Feb_26_2022 from "./images/Feb_26_2022.jpg";
+import Mar_21_2022 from "./images/Mar_21_2022.jpg";
+import May_7_2022 from "./images/May_7_2022.jpg";
+import May_8_2022 from "./images/May_8_2022.jpg";
+import Jun_17_2022 from "./images/Jun_17_2022.jpg";
+import Sep_8_2022 from "./images/Sep_8_2022.jpg";
+import Oct_22_2022 from "./images/Oct_22_2022.jpg";
+import Dec_2_2022 from "./images/Dec_2_2022.jpg";
 
 const timelineData = [
   {
@@ -70,7 +85,7 @@ const timelineData = [
     date: "2020-09-11 00:00:00 EST",
     title: "First 'I Love You'",
     description:
-      "First time we dressed up any when out for our 6-month anniversary. We sat on a bench after dinner and I told you that I loved you. You said it back too fast!",
+      "First time we dressed up and when out for our 6-month anniversary. We sat on a bench after dinner and I told you that I loved you. You said it back too fast!",
     icon: <FaHeart />,
     image: Sep_11_2020,
   },
@@ -145,6 +160,78 @@ const timelineData = [
       "A beautiful day. We've known each other for less than 18 months but we knew in our hearts we were right for each other.",
     icon: <FaRing />,
     image: Aug_12_2021,
+  },
+  {
+    date: "2022-02-19 00:00:00 EST",
+    title: "Caleb is born!",
+    description:
+      "After a very stressful labour, we get to meet our little Babu. I barely sleep the first night in the hospital and we're surprised they just let us take our baby home! I'll never forget the ride home - playing 'Hey Jude' and cherishing our first ride together home as a family.",
+    icon: <FaBaby />,
+    image: Feb_12_2022,
+  },
+  {
+    date: "2022-02-26 00:00:00 EST",
+    title: "The Bris",
+    description:
+      "Caleb's bris was a little stressful but smooth sailing. Most exciting part was your dad almost falling asleep at the wheel on the way home.",
+    icon: <FaStarOfDavid />,
+    image: Feb_26_2022,
+  },
+  {
+    date: "2022-03-21 00:00:00 EST",
+    title: "He's the Savior!",
+    description:
+      "We have to buy Caleb back from the Rabbi because he is the savior of humanity. We're pretty sure the Rabbi was looking up the ceremony on Wikipedia when we talked to her about it on the phone. ",
+    icon: <FaSynagogue />,
+    image: Mar_21_2022,
+  },
+  {
+    date: "2022-05-07 00:00:00 EST",
+    title: "Walks Downtown",
+    description:
+      "One of our first walks downtown with children. The college girls at Bungalow thought Caleb was adorable. I always love thinking about this walk and hope we have so many more.",
+    icon: <FaWalking />,
+    image: May_7_2022,
+  },
+  {
+    date: "2022-05-08 00:00:00 EST",
+    title: "First Mother's Day",
+    description:
+      "Caleb stays at home so we can go to Idylwyld and have delicious scallops.",
+    icon: <FaHeart />,
+    image: May_8_2022,
+  },
+  {
+    date: "2022-05-08 00:00:00 EST",
+    title: "A dapper little boy",
+    description:
+      "My cousin's wedding and our first time to show-off little man in a suit. He's a big hit and we get a chance to go to a wedding as a couple.",
+    icon: <FaRing />,
+    image: Jun_17_2022,
+  },
+  {
+    date: "2022-05-08 00:00:00 EST",
+    title: "Family Photos",
+    description:
+      "Our first family photo session! Could have gone better since Caleb didn't smile until the very end but we still got some great shots of his amazing smile.",
+    icon: <FaCamera />,
+    image: Sep_8_2022,
+  },
+  {
+    date: "2022-05-08 00:00:00 EST",
+    title: "California Hiking",
+    description:
+      "The first big trip with a child. We barely make our connecting flight in Calgary but it all works out. That hiking bag was a great investment!",
+    icon: <FaHiking />,
+    image: Oct_22_2022,
+  },
+  {
+    date: "2022-05-08 00:00:00 EST",
+    title: "Florida is the best!",
+    description:
+      "Even though we're living in a construction zone Florida is still pretty fantastic. We decide we're going to retire here and start our own family bootcamp (a la Granny).",
+    icon: <FaUmbrellaBeach />,
+    image: Dec_2_2022,
   },
 ];
 
